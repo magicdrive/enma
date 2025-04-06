49c49c628b1f8435aeaa5ef0cf9d80cfe3d38dcd
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2025-04-06 10:11:47 +0900

    Added help message

--- cmd/enma/mod.go
+++ cmd/enma/mod.go
@@ -5,9 +5,15 @@ import (
 	"os"
 
 	"github.com/magicdrive/enma/internal/commandline"
+	"github.com/magicdrive/enma/internal/common"
 )
 
 func Execute(version string) {
+	if len(os.Args) >= 1 {
+		common.EnmaHelpFunc()
+		os.Exit(0)
+	}
+
 	err := commandline.Execute(version, os.Args[1:])
 	if err != nil {
 		log.Fatalf("Faital Error: %v\n", err)
